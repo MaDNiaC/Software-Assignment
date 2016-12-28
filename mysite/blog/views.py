8533bbe943c75d711c1ce5dff37b6813ce68fa23
MaDNiaC <eneskaplan@std.sehir.edu.tr>
2016-12-28 11:47:10 +0200

    Software-Assignment-4
    CS 361 Software Assignment 4

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,26 +8,31 @@ from django.shortcuts import render
 
 from tags.models import Tag
 from .models import blogEntry
+from .forms import BlogForm
 
 def show_entries(request):
 
     if request.method == "POST":
-        entry = blogEntry.objects.create(name=request.POST.get("todo_name"),
-                            description=request.POST.get("description_name"),
-                            owner=request.user)
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            entry = form.save(commit=False)
+            entry.owner = request.user
+            entry.save()
+            form.save_m2m()
 
-        entry.tags.add(*request.POST.getlist("tag_names"))
-
+    elif request.method == "GET":
+        form = BlogForm()
 
     return render(request, "my_entries.html", {"todos": blogEntry.objects.filter(owner=request.user.id),
-                                             "tags":Tag.objects.all()})
+                                             "tags":Tag.objects.all(),
+                                             "form": form})
 
 
-def get_entry(request, todo_id):
+def get_entry(request, entry_id):
     try:
         entry = blogEntry.objects.get(id=entry_id)
-        if request.user.id != entry.owner.id:
+        if request.user.id != todo.owner.id:
             raise PermissionDenied
-        return render(request, "detailed_entries.html", {"todo": entry})
+        return render(request, "detailed_entry.html", {"todo": entry})
     except blogEntry.DoesNotExist:
         raise Http404("We don't have any.")
